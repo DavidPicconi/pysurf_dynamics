8b55c85860b53e7937e725cb681f33021624f3b8
David Picconi <124689239+DavidPicconi@users.noreply.github.com>
2024-11-25 12:19:30 +0100

    Added reference to the plugins directory

--- pysurf/__init__.py
+++ pysurf/__init__.py
@@ -14,8 +14,12 @@ from .spp.spp import SurfacePointProvider, get_spp
 from .spp import  AbinitioBase, Model, Interpolator
 # load plugins
 base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-core_plugins = os.path.join(base, "core_plugins")
-user_plugins = os.path.join(base, "plugins")
+with open(os.path.join(base, 'pysurf', 'source_path.txt'), 'r') as fh:
+    src_base = fh.readlines()[0]
+#core_plugins = os.path.join(base, "core_plugins")
+#user_plugins = os.path.join(base, "plugins")
+core_plugins = os.path.join(src_base, "core_plugins")
+user_plugins = os.path.join(src_base, "plugins")
 # load core plugins
 PluginLoader(core_plugins, ignorefile='plugins.ini')
 # load user plugins
